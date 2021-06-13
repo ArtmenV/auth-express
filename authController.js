17ebcd7cd3b66feb61a56499e8f07772c34a746f
ArtmenV <viliutich.artem@gmail.com>
2021-06-13 19:59:04 +0700

    done registration

--- authController.js
+++ authController.js
@@ -1,12 +1,33 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
+const bcrypt = require('bcryptjs');
 
 class AuthController {
   async registration(req, res) {
     try {
+      const {username, password} = req.body
 
-    } catch (e) {s
+      const candidate = await User.findOne({username})
+      if (candidate) {
+        return res.status(400).json({
+          message: 'Пользователь с таким именем уже существует'
+        })
+      }
 
+      const hashPassword = bcrypt.hashSync(password, 6)
+      const userRole = await Role.findOne({value: "USER"})
+      const user = new User({
+        username, 
+        password: hashPassword,
+        roles: [userRole.value]
+      })
+      await user.save()
+      return res.json({message: "Пользователь был успешно зарегстрирован"})
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({
+        message: 'Registration error'
+      })
     }
   }
 
@@ -14,16 +35,15 @@ class AuthController {
     try {
 
     } catch (e) {
-
+      console.log(e);  
+      res.status(400).json({
+        message: 'Login error'
+      })
     }
   }
 
   async getUsers(req, res) {
     try {
-      const userRole = new Role()
-      const adminRole = new Role({ value: "ADMIN" })
-      await userRole.save()
-      await adminRole.save()
       res.json('server work')
     } catch (e) {
 
