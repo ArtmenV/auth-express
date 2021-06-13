3a8e6ecf4008b73b8a6bafdf8a586ee9e393966c
ArtmenV <viliutich.artem@gmail.com>
2021-06-13 21:48:43 +0700

    work with roles

--- authController.js
+++ authController.js
@@ -1,8 +1,17 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const { validationResult } = require('express-validator')
+const { secret } = require("./config")
 
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles
+  }
+  return jwt.sign(payload, secret, {expiresIn: '24h'})
+}
 class AuthController {
   async registration(req, res) {
     try {
@@ -48,7 +57,15 @@ class AuthController {
           message: `Пользователь с таким ${username} не найден`
         })
       }
+      const validPassword = bcrypt.compareSync(password, user.password)
+      if (!validPassword) {
+        return res.status(400).json({
+          message: 'Введен неверный пароль'
+        })
+      }
 
+      const token = generateAccessToken(user._id, user.roles)
+      return res.json({token})
     } catch (e) {
       console.log(e);  
       res.status(400).json({
@@ -59,7 +76,8 @@ class AuthController {
 
   async getUsers(req, res) {
     try {
-      res.json('server work')
+      const users = await User.find()
+      res.json(users)
     } catch (e) {
 
     }
