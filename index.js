4eb044b77704347cc4543d2f9df895fa96fcd515
ArtmenV <viliutich.artem@gmail.com>
2021-06-13 19:01:22 +0700

    done with routers

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const authRouter = require('./authRouter')
+const { connect } = require('mongoose')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use('/auth', authRouter)
 
-const start = () => {
+const start = async () => {
   try {
+    await connect(`mongodb+srv://artUser:art123@authcluster.ece86.mongodb.net/auth?retryWrites=true&w=majority`, 
+      { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+      }
+    );
     app.listen(PORT, () => console.log(`server started on port ${PORT}`))
   } catch (e) {
 
